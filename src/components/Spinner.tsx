133ef8651e935526cac39d486ee9e5d5861f74fe
MonaRuz <mona.ruz@seznam.cz>
2024-11-19 09:09:55 +0100

    Typed Spinner component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 import { BarLoader } from "react-spinners";
 
 
-export default function Spinner({children}) {
+export default function Spinner({children}:{children:React.ReactNode}) {
   return (
     <div className="flex flex-col items-center gap-4 mt-10">
         <p className="text-pink-200">Loading {children}...</p>
