4b40404492910261613e280caf4bbd507a61f299
MonaRuz <mona.ruz@seznam.cz>
2024-11-19 09:06:29 +0100

    Typed Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,10 @@
-export default function Button({ style, children, onClick }) {
+type ButtonProps={
+	style:{};
+	children:React.ReactNode;
+	onClick:()=>void;
+}
+
+export default function Button({ style, children, onClick }:ButtonProps) {
 	return (		
 			<button className="border-4 border-black hover:border-none" onClick={onClick} style={style}>{children}</button>
 		
